58855eca1f98228f8eaf071809ef880a08ce49dd
kimia <doulabikimia@gmail.com>
2025-04-25 14:34:57 -0400

    I tried to redo 	Q25

--- CountMaxFever.java
+++ CountMaxFever.java
@@ -9,36 +9,43 @@ import java.util.ArrayList;
 public class CountMaxFever
 {
     // instance variables - replace the example below with your own
-    private int temperature; 
-
+    private String fever; 
+    private ArrayList<Double> temperatures;
+    
      /**
      * Constructor for objects of class CountMaxFever
      */
     public CountMaxFever()
     {
-        ArrayList<String> temperature = new ArrayList<>();
-        temperature = 30; 
-        trmperature = 40;
+        ArrayList<Double>temperatures = new ArrayList<>();
+        
+        temperatures.add(37.2);
+        temperatures.add(40.2);
+        temperatures.add(39.2);
+        temperatures.add(22.2);
+        
+        fever(temperatures);
     }
     //Q25
-     private void countFeverCount() {
-         for(temperature i = 0; i> 37.5; i++){
-           if(i++>i){
-                 system.out.println(i + "maximum");
-            }
-           system.out.println(i.ArrayList);
-        }
-        //Q27 
+    private String fever(ArrayList<Double>temperatures) {
+       for(double i = 0; i> 37.5; i++){
+           System.out.println(i);
+       }
+       return("fever : "+ );}
+    
+       
+    //Q27 
         for (temperature i > current > i=++){
-          system.out.println(collections.sort); 
-        }
-        //Q26
-        //i would write same code but for the print i would write
+          system.out.println(collections.sort);} 
+    
+        Q26
+        i would write same code but for the print i would write
         system.out.println(collection.maxi);
     }
-    
-    
 }
+    
+    
+
     
 
 
