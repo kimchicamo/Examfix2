98a2cc2758a6091be3b99131b362e0d44c35a988
kimia <doulabikimia@gmail.com>
2025-04-25 13:57:39 -0400

    Initial sharing of project

---
+++ CountMaxFever.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+/**
+ * Write a description of class CountMaxFever here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class CountMaxFever
+{
+    // instance variables - replace the example below with your own
+    private int temperature; 
+
+     /**
+     * Constructor for objects of class CountMaxFever
+     */
+    public CountMaxFever()
+    {
+        ArrayList<String> temperature = new ArrayList<>();
+        temperature = 30; 
+        trmperature = 40;
+    }
+    //Q25
+     private void countFeverCount() {
+         for(temperature i = 0; i> 37.5; i++){
+           if(i++>i){
+                 system.out.println(i + "maximum");
+            }
+           system.out.println(i.ArrayList);
+        }
+        //Q27 
+        for (temperature i > current > i=++){
+          system.out.println(collections.sort); 
+        }
+        //Q26
+        //i would write same code but for the print i would write
+        system.out.println(collection.maxi);
+    }
+    
+    
+}
+    
+
+
---
+++ Names.java
@@ -0,0 +1,37 @@
+
+/**
+ * Write a description of class Names here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Names
+{
+    private int x;
+
+    /**
+     * Constructor for objects of class Names
+     */
+    public Names()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public void sampleMethod()
+    {
+       if(name.equal sara){
+         system.out.println("name : " + sara );}
+       else(namsysee.equal kimia){
+         system.out.print("name" + kimia);
+    }
+         
+}
+    
+
